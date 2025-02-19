f4505cc315cdb02d74a6a25c8f385fbe0e562a4d
Disha <dishakamble24@gmail.com>
2025-02-19 15:42:58 +0530

    Main Method

---
+++ DataAnnotationPractice/Program.cs
@@ -0,0 +1,14 @@
+﻿using DataAnnotationPractice;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(" Data Annotation Practice!");
+
+        /*Validations.ValidateStudentInformation();
+        Validations.ValidateEmployeeInformation();*/
+        Validations.ValidateUserLogin();
+
+    }
+}
