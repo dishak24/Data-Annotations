de3c608fbb5017cf660a99264ffeb25a93d7ffb0
Disha <dishakamble24@gmail.com>
2025-02-19 15:46:01 +0530

    Employee Class

---
+++ DataAnnotationPractice/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAnnotationPractice
+{
+    internal class Employee
+    {
+        [Required(ErrorMessage = "ID is required!")]
+        [Range(100, int.MaxValue, ErrorMessage = "Length of ID must be starts from 100 !")]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Name is required!")]
+        [StringLength(20,MinimumLength =2, ErrorMessage = "Please enter atleast 2 letters initial of name !")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required!")]
+        [EmailAddress( ErrorMessage = "Invalid email format !")]
+        public string Email { get; set; }
+
+
+
+         
+
+
+    }
+}
