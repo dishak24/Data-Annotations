f4d00d9b9ef8acb8376337f3c105aa8e8b89e2a9
Disha <dishakamble24@gmail.com>
2025-02-19 15:46:33 +0530

    Student Class

---
+++ DataAnnotationPractice/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAnnotationPractice
+{
+    internal class Student
+    {
+        [Required(ErrorMessage =" Name is Required !")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Please enter atleast 2 letters initial of name !")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = " Age is Required !")]
+        [Range(15, 28, ErrorMessage = "Please enter age between 15 - 28!")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = " Percentage is Required !")]
+        [Range(35, 100, ErrorMessage = "Please enter valid percentage!")]
+        public double Percentage { get; set; }
+    }
+}
